db57e98fba619c968f72fb1055ae34e6e17f1591
dalance <dalance@gmail.com>
2016-01-16 23:03:29 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone https://github.com/rust-lang/cargo.git
 cd rust
 cp ../src/i586-unknown-linux-gnu.mk ./mk/cfg
 cp ../src/i586-unknown-linux-gnu.rs ../../rust/src/librustc_back/target
-./configure --target=i586-unknown-linux-gnu --host=i586-unknown-linux-gnu --build=i586-unknown-linux-gnu
+./configure --target=i586-unknown-linux-gnu --build=i586-unknown-linux-gnu
 make
 cd ..
 
