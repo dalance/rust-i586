acd08d17826b3fcac3fcb8e093c0f171d6547f6a
dalance <dalance@gmail.com>
2016-01-18 01:23:15 +0900

    add target

--- build.sh
+++ build.sh
@@ -28,6 +28,7 @@ cd rust
 git checkout $branch
 cp ../src/i586-unknown-linux-gnu.mk ./mk/cfg
 cp ../src/i586_unknown_linux_gnu.rs ./src/librustc_back/target
+cp ../src/mod.rs ./src/librustc_back/target
 cp ../src/snapshot.py ./src/etc
 cp ../src/get-snapshot.py ./src/etc
 ./configure --target=i586-unknown-linux-gnu --host=i586-unknown-linux-gnu --build=i586-unknown-linux-gnu
