67bd969c03418eb207e0d640bc2477876b85970c
dalance <dalance@gmail.com>
2016-01-17 11:25:35 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 export CFLAGS=-march=pentium2
 export CXXFLAGS=-march=pentium2
 
-if [[ ! -e rust ]]; then git clone https://github.com/rust-lang/rust.git; fi
+if [ ! -e rust ]; then git clone https://github.com/rust-lang/rust.git; fi
 git clone https://github.com/rust-lang/cargo.git
 
 cd rust
 cp ../src/i586-unknown-linux-gnu.mk ./mk/cfg
 cp ../src/i586_unknown_linux_gnu.rs ./src/librustc_back/target
 cp ../src/snapshot.py ./src/etc
-./configure --enable-dist-host-only --target=i586-unknown-linux-gnu --host=i586-unknown-linux-gnu --build=i686-unknown-linux-gnu
+./configure --target=i586-unknown-linux-gnu --host=i586-unknown-linux-gnu --build=i686-unknown-linux-gnu
 make
 cd ..
 
