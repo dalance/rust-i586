cb432f2cf9e5ccc1a69e31c5d32df2247b708673
dalance <dalance@gmail.com>
2016-01-18 15:22:54 +0900

    fix setup_rust.sh

--- setup_rust.sh
+++ setup_rust.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/rust-lang/rust.git
 
 cd rust
-git checkout `cat branch`
+git checkout `cat ../branch`
 cp ../src/i586-unknown-linux-gnu.mk ./mk/cfg
 cp ../src/i586_unknown_linux_gnu.rs ./src/librustc_back/target
 cp ../src/mod.rs ./src/librustc_back/target
