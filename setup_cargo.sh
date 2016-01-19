dc5b09d2f1f1585ef335299c1232bed3dfedad33
dalance <dalance@gmail.com>
2016-01-19 13:45:36 +0900

    update

--- setup_cargo.sh
+++ setup_cargo.sh
@@ -5,5 +5,12 @@ git clone https://github.com/rust-lang/cargo.git
 cd cargo
 git submodule update --init
 python -B src/etc/install-deps.py
-./configure --target=i586-unknown-linux-gnu --local-rust-root=/usr/local
+./configure --target=i586-unknown-linux-gnu --build=i586-unknown-linux-gnu --host=i586-unknown-linux-gnu --local-rust-root=/usr/local
+make
+mkdir ../mods
+cp -r ~/.cargo/registry/src/github.com-*/* ../mods/
+mkdir .cargo
+ls -d1 `pwd`/../mods/* | awk 'BEGIN{print "paths = ["}//{print "\""$0"\","}END{print "]"}' > .cargo.config
+grep -lr target.i686-unknown-linux-gnu.dependencies ../mods | xargs sed -i.bak -e 's/target.i686-unknown-linux-gnu.dependencies/target.i586-unknown-linux-gnu.dependencies/g'
+#grep -lr rustc-link-search ../mods | xargs sed -i.bak -e 's/target.i686-unknown-linux-gnu.dependencies/target.i586-unknown-linux-gnu.dependencies/g'
 cd ..
