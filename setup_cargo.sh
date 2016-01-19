893e6221a461e0e9bacb90c614fdd456923ef94f
dalance <dalance@gmail.com>
2016-01-19 21:47:13 +0900

    update setup_cargo.sh

--- setup_cargo.sh
+++ setup_cargo.sh
@@ -10,7 +10,7 @@ make
 mkdir ../mods
 cp -r ~/.cargo/registry/src/github.com-*/* ../mods/
 mkdir .cargo
-grep -lr target.i686-unknown-linux-gnu.dependencies `pwd`/../mods | awk 'BEGIN{print "paths = ["}//{print "\""gsub("Cargo.toml","",$0)"\","}END{print "]"}' > .cargo/config
+grep -lr target.i686-unknown-linux-gnu.dependencies `pwd`/../mods | awk 'BEGIN{print "paths = ["}//{out=$0;gsub("Cargo.toml","",$out);print "\""$out"\","}END{print "]"}' > .cargo/config
 grep -lr target.i686-unknown-linux-gnu.dependencies ../mods | xargs sed -i.bak -e 's/target.i686-unknown-linux-gnu.dependencies/target.i586-unknown-linux-gnu.dependencies/g'
 grep -lr "curl-sys = { path = \"curl-sys\", version = \"0.1.0\" }" ../mods | xargs sed -i.bak -e 's/curl-sys = { path = \"curl-sys\", version = \"0.1.0\" }/curl-sys = \"0.1.0\"/g'
 make
