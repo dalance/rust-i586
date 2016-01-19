cce4565beaee0c721af2aa2e5824eff658242f5a
dalance <dalance@gmail.com>
2016-01-19 16:49:27 +0900

    update cargo build

--- clean.sh
+++ clean.sh
@@ -3,8 +3,9 @@
 mkdir -p result
 cp rust/LICENSE* rust/COPYRIGHT cargo/LICENSE* result
 cp -r rust/i586-unknown-linux-gnu/stage2/* result
+cp cargo/target/i586-unknown-linux-gnu/release/cargo result
 cd result
-tar jcvf `cat tarball` ./
-cp `cat tarball` $CIRCLE_ARTIFACTS
+tar jcvf `cat ../tarball` ./
+cp `cat ../tarball` $CIRCLE_ARTIFACTS
 cd ..
 rm -rf rust cargo
--- setup_cargo.sh
+++ setup_cargo.sh
@@ -10,7 +10,7 @@ make
 mkdir ../mods
 cp -r ~/.cargo/registry/src/github.com-*/* ../mods/
 mkdir .cargo
-ls -d1 `pwd`/../mods/* | awk 'BEGIN{print "paths = ["}//{print "\""$0"\","}END{print "]"}' > .cargo.config
+grep -lr target.i686-unknown-linux-gnu.dependencies `pwd`/../mods | awk 'BEGIN{print "paths = ["}//{print "\""$0"\","}END{print "]"}' > .cargo/config
 grep -lr target.i686-unknown-linux-gnu.dependencies ../mods | xargs sed -i.bak -e 's/target.i686-unknown-linux-gnu.dependencies/target.i586-unknown-linux-gnu.dependencies/g'
-#grep -lr rustc-link-search ../mods | xargs sed -i.bak -e 's/target.i686-unknown-linux-gnu.dependencies/target.i586-unknown-linux-gnu.dependencies/g'
+grep -lr rustc-link-search=native=/usr/lib/x86_64-linux-gnu ./ | xargs sed -i.bak -e 's/rustc-link-search=native=/usr/lib/x86_64-linux-gnu/rustc-link-search=native=/usr/lib32/g'
 cd ..
