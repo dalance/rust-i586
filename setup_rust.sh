49d83f254e2cc8e69610d59d978a02c0c5a1328b
dalance <dalance@gmail.com>
2016-01-20 10:18:23 +0900

    remove unused files

--- setup_rust.sh
+++ setup_rust.sh
@@ -6,8 +6,8 @@ cd rust
 git checkout `cat ../branch`
 cp ../src/i586-unknown-linux-gnu.mk ./mk/cfg
 cp ../src/i586_unknown_linux_gnu.rs ./src/librustc_back/target
-cp ../src/snapshot.py ./src/etc
 cp ../src/get-snapshot.py ./src/etc
 echo ./src/librustc_back/target/mod.rs | xargs sed -i.bak -e 's/i686_unknown_linux_gnu/i586_unknown_linux_gnu/g'
+echo ./src/etc/snapshot.py | xargs sed -i.bak -e 's/i686/i586/g'
 ./configure --target=i586-unknown-linux-gnu --host=i586-unknown-linux-gnu --build=i586-unknown-linux-gnu
 cd ..
